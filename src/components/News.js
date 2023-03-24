56aaa94b2b9b67dba374386551128bb76c0334d7
Rajveer <edu.rajveersinh@gmail.com>
2023-03-24 12:12:32 +0530

    Added Infinitescroll

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from '../media/Spinner';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import PropTypes from 'prop-types'
 
 
@@ -26,7 +27,8 @@ export default class News extends Component {
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
+      totalResults: 0
     }
 
 
@@ -35,7 +37,7 @@ export default class News extends Component {
 
   async updateNews() {
     let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=5469e571a9c44c6c8037ce15909fff46&pageSize=${this.props.pageSize}&page=${this.state.page}`;
-    this.setState({ loading: true })
+    // this.setState({ loading: true })
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
@@ -55,50 +57,44 @@ export default class News extends Component {
   }
 
 
-  handleNext = async () => {
 
-    // let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=5469e571a9c44c6c8037ce15909fff46&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-    // this.setState({ loading: true })
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // this.setState({ page: this.state.page + 1,articles: parsedData.articles,loading: false })
-    this.setState({ page: this.state.page + 1 });
-    this.updateNews();
-  }
-
-  handlePrevious = async () => {
-    // let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=5469e571a9c44c6c8037ce15909fff46&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    // this.setState({ loading: true })
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // this.setState({page: this.state.page - 1,articles: parsedData.articles,loading: false})
-
-    this.setState({ page: this.state.page - 1 });
-    this.updateNews();
-  }
-
-
+  fetchMoreData = async () => {
+    // this.setState({ page: this.state.page + 1});
+    let url = `https://newsapi.org/v2/top-headlines?category=${this.props.category}&country=${this.props.country}&apiKey=5469e571a9c44c6c8037ce15909fff46&pageSize=${this.props.pageSize}&page=${this.state.page}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: this.state.articles.concat(parsedData.articles), totalResults: this.state.totalResults});
+  };
 
   render() {
     return (
 
-      <div className="container my-3">
+      <>
         <h1 className="text-center">NewsFire - Top Headlines Of The Day</h1>
         {this.state.loading && <Spinner />}
-        <div className="row d-flex justify-content-start" >
-          {!this.state.loading && this.state.articles.map((element) => {
-            return <div className="col-md-3 my-3" key={element.url}  >
-              <NewsItem title={element.title ? element.title.slice(0, 75) : " "} description={element.description ? element.description.slice(0,100) : ""} imageurl={element.urlToImage ? element.urlToImage : "https://resize.indiatvnews.com/en/resize/newbucket/730_-/2020/11/breaking-1603159815-1606179737.jpg"} newsurl={element.url ? element.url : " "} author={element.author ? element.author.slice(0,20) : "Unknown"} date={element.publishedAt} source={element.source.name} />
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.totalResults !== this.state.totalResults}
+          loader={<h4>{<Spinner />}</h4>}
+        >
+          <div className="container">
+
+            <div className="row d-flex justify-content-start" >
+              {this.state.articles.map((element) => {
+                return <div className="col-md-3 my-3" key={element.url}  >
+                  <NewsItem title={element.title ? element.title.slice(0, 75) : " "} description={element.description ? element.description.slice(0, 100) : ""} imageurl={element.urlToImage ? element.urlToImage : "https://resize.indiatvnews.com/en/resize/newbucket/730_-/2020/11/breaking-1603159815-1606179737.jpg"} newsurl={element.url ? element.url : " "} author={element.author ? element.author.slice(0, 20) : "Unknown"} date={element.publishedAt} source={element.source.name} />
+                </div>
+              })}
+
+
             </div>
-          })}
+          </div>
+        </InfiniteScroll>
 
-
-        </div>
-        <div className="container d-flex justify-content-between my-3">
-          <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevious}>&larr; Previous</button>
-          <button disabled={this.state.page + 1 > this.state.totalResults / this.props.pageSize} type="button" className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
-        </div>
-      </div>
+      </>
     )
   }
 }
